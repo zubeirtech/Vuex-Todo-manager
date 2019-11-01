4b3c1006f0d5ad1954e4e87faadc4732d8c1718c
Zubeir Mohamed <zumo@solute.de>
2019-11-01 23:41:10 +0100

    add and finish project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import store from './store';
+
+library.add(faUserSecret, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
